09467f560086bd1aebda0ea70908f48220b626b0
okaasantoneko <arukehashire@gmail.com>
2022-03-13 22:15:44 +0900

    add script

---
+++ japanpost-shortcut.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         japanpost-shortcut
+// @namespace    https://github.com/okaasantoneko
+// @version      0.1
+// @description  enable shortcuts on JapanPost websites
+// @author       okaasantoneko
+// @match        https://trackings.post.japanpost.jp/*
+// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+$('document').off()
